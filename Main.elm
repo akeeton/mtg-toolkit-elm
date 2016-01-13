9bb4c3919d4d3c8c02e4db1aae925c22ebe85946
Andrew Keeton <akeeton@users.noreply.github.com>
2016-01-13 15:10:58 -0500

    Bring in some partial AllCards.json

--- Main.elm
+++ Main.elm
@@ -7,13 +7,20 @@ main =
     StartApp.start { model = model, view = view, update = update }
 
 
-model = 0
+partialAllCardsJson = """{"Air Elemental":{"layout":"normal","name":"Air Elemental","manaCost":"{3}{U}{U}","cmc":5,"colors":["Blue"],"type":"Creature — Elemental","types":["Creature"],"subtypes":["Elemental"],"text":"Flying","power":"4","toughness":"4","imageName":"air elemental","colorIdentity":["U"]},"Ancestral Recall":{"layout":"normal","name":"Ancestral Recall","manaCost":"{U}","cmc":1,"colors":["Blue"],"type":"Instant","types":["Instant"],"text":"Target player draws three cards.","imageName":"ancestral recall","colorIdentity":["U"]}}"""
+
+type alias Model =
+    { allCardsJson : String
+    }
+
+model : Model
+model = { allCardsJson = partialAllCardsJson }
 
 
 view address model =
     div []
         [ button [ onClick address Decrement  ] [ text "-"  ]
-        , div [] [ text (toString model)  ]
+        , div [] [ text model.allCardsJson  ]
         , button [ onClick address Increment  ] [ text "+"  ]
         ]
 
@@ -23,5 +30,6 @@ type Action = Increment | Decrement
 
 update action model =
     case action of
-        Increment -> model + 1
-        Decrement -> model - 1
+        Increment -> { model | allCardsJson = "Increment" }
+        Decrement -> { model | allCardsJson = "Decrement" }
+
